2b8a7d17e7b7e5a10c0b912035775ac8f84934b9
ChrisClems <neilclemons@gmail.com>
2024-05-30 12:38:05 -0700

    Added helper functions to AppHelper to stop running Solid Edge processes in various ways. Added README.md

--- EdgeSharp/AppHelper.cs
+++ EdgeSharp/AppHelper.cs
@@ -1,18 +1,23 @@
 ﻿using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
+using System.Diagnostics;
 using SolidEdgeFramework;
+using SolidEdgeSDK;
 
 namespace EdgeSharp;
 
 public class AppHelper
 {
-
+    /// <summary>
+    /// Checks if Solid Edge application is running.
+    /// </summary>
+    /// <returns>Returns true if Solid Edge application is running, otherwise false.</returns>
     public static bool IsSolidEdgeRunning()
     {
         try
         {
-            var app = (Application)Marshal.GetActiveObject("SolidEdge.Application");
-            return true;
+            var comObjectHandle = Marshal.GetActiveObject("SolidEdge.Application");
+            return comObjectHandle != null;
         }
         catch
         {
@@ -20,4 +25,63 @@ public class AppHelper
         }
     }
 
+
+    /// <summary>
+    /// Stops the first found running instance of Solid Edge gracefully.
+    /// </summary>
+    public static void StopSolidEdge()
+    {
+        try
+        {
+            var app = (Application)Marshal.GetActiveObject(PROGID.SolidEdge_Application);
+            app.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+            app = null;
+        }
+        catch
+        {
+            // ignored
+        }
+    }
+
+    /// <summary>
+    /// Stops specific running instance of Solid Edge application gracefully.
+    /// </summary>
+    /// <param name="app">The Solid Edge application instance to stop.</param>
+    public static void StopSolidEdge(Application app)
+    {
+        app.Quit();
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+        app = null;
+    }
+
+    /// <summary>
+    /// Stops all running instances of Solid Edge gracefully.
+    /// </summary>
+    public static void StopAllSolidEdge()
+    {
+        while (IsSolidEdgeRunning())
+        {
+            StopSolidEdge();
+        }
+    }
+
+    /// <summary>
+    /// Kills all running Solid Edge processes.
+    /// </summary>
+    public static void KillAllSolidEdgeProcs()
+    {
+        foreach (var process in Process.GetProcesses())
+        {
+            if (!process.ProcessName.StartsWith("Edge.exe")) continue;
+            try
+            {
+                process.Kill();
+            }
+            catch (Exception)
+            {
+                // Ignore
+            }
+        }
+    }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# EdgeSharp
+
+EdgeSharp is a .NET library for interacting with Solid Edge. It is the spiritual successor to SolidEdge.Community, build on modern .NET.
+
+SolidEdge.Community extensions will be slowly added as they're vetted for need in modern versions of Solid Edge.
