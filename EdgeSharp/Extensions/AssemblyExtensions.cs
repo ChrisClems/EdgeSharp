4a6568f41ed32a87ca64dbfba24eeb51d6c4e69a
ChrisClems <neilclemons@gmail.com>
2024-06-05 15:21:17 -0700

    Initial implementation of TraverseOccurrencesWithAction extension method to use delegate to alter assembly occurrences

---
+++ EdgeSharp/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,62 @@
+﻿using SolidEdgeAssembly;
+using SolidEdgeFramework;
+using SolidEdgePart;
+
+namespace EdgeSharp.Extensions;
+
+public static class AssemblyExtensions
+{
+    public static void TraverseOccurencesWithAction(this AssemblyDocument asm, Action<AssemblyDocument> action, bool recursive)
+    {
+        var occurrences = asm.Occurrences;
+        foreach (Occurrence occurrence in occurrences)
+        {
+            var document = occurrence as AssemblyDocument;
+            if (document == null)
+            {
+                continue;
+            }
+
+            action(document);
+        }
+        
+    }
+    
+    public static void TraverseOccurencesWithAction(this AssemblyDocument asm, Action<PartDocument> action, bool recursive)
+    {
+        var occurrences = asm.Occurrences;
+        foreach (Occurrence occurrence in occurrences)
+        {
+            var document = (SolidEdgeDocument)occurrence;
+
+            switch (document.Type)
+            {
+                case DocumentTypeConstants.igAssemblyDocument:
+                    // recurive
+                case DocumentTypeConstants.igPartDocument:
+                    // action
+                default:
+                    continue;
+            }
+        }
+        
+    }
+    
+    public static void TraverseOccurencesWithAction(this AssemblyDocument asm, Action<SheetMetalDocument> action, bool recursive)
+    {
+        var occurrences = asm.Occurrences;
+        foreach (Occurrence occurrence in occurrences)
+        {
+            var document = occurrence as SheetMetalDocument;
+            if (document == null)
+            {
+                continue;
+            }
+
+            action(document);
+        }
+        
+    }
+    
+    
+}
