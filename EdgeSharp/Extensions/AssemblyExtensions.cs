e9ec7941b1ae7a14820fab5727126525441ebe05
Chris <neilclemons@gmail.com>
2024-06-05 19:22:59 -0700

    Filled out the TraverseOccurrenceWithAction extension methods.

--- EdgeSharp/Extensions/AssemblyExtensions.cs
+++ EdgeSharp/Extensions/AssemblyExtensions.cs
@@ -6,7 +6,9 @@ namespace EdgeSharp.Extensions;
 
 public static class AssemblyExtensions
 {
-    public static void TraverseOccurencesWithAction(this AssemblyDocument asm, Action<PartDocument> action, bool recursive)
+    # region TraverseOccurencesWithAction
+    // Process all Solid Edge documents in assembly using delegate action
+    public static void TraverseOccurrencesWithAction(this AssemblyDocument asm, Action<SolidEdgeDocument> action, bool recursive)
     {
         var occurrences = asm.Occurrences;
         foreach (Occurrence occurrence in occurrences)
@@ -16,13 +18,68 @@ public static class AssemblyExtensions
             switch (document.Type)
             {
                 case DocumentTypeConstants.igAssemblyDocument:
-                    // recurive
-                case DocumentTypeConstants.igPartDocument:
-                    // action
+                    // If recursion enabled, reprocess assembly
+                    if (!recursive) continue;
+                    var asmDoc = (AssemblyDocument)document;
+                    asmDoc.TraverseOccurrencesWithAction(action, recursive);
+                    break;
                 default:
-                    continue;
+                    // Process document with action delegate
+                    action(document);
+                    break;
             }
         }
-        
     }
+    
+    // Process all part documents in assembly using delegate action
+    public static void TraverseOccurrencesWithAction(this AssemblyDocument asm, Action<PartDocument> action, bool recursive)
+    {
+        var occurrences = asm.Occurrences;
+        foreach (Occurrence occurrence in occurrences)
+        {
+            var document = (SolidEdgeDocument)occurrence;
+
+            switch (document.Type)
+            {
+                case DocumentTypeConstants.igAssemblyDocument:
+                    // If recursion enabled, reprocess assembly
+                    if (!recursive) continue;
+                    var asmDoc = (AssemblyDocument)document;
+                    asmDoc.TraverseOccurrencesWithAction(action, recursive);
+                    break;
+                case DocumentTypeConstants.igPartDocument:
+                    // Process part document with action delegate
+                    var partDoc = (PartDocument)document;
+                    action(partDoc);
+                    break;
+            }
+        }
+    }
+    
+    // Process all sheet metal documents in assembly using delegate action
+    public static void TraverseOccurrencesWithAction(this AssemblyDocument asm, Action<SheetMetalDocument> action, bool recursive)
+    {
+        var occurrences = asm.Occurrences;
+        foreach (Occurrence occurrence in occurrences)
+        {
+            var document = (SolidEdgeDocument)occurrence;
+
+            switch (document.Type)
+            {
+                case DocumentTypeConstants.igAssemblyDocument:
+                    // If recursion enabled, reprocess assembly
+                    if (!recursive) continue;
+                    var asmDoc = (AssemblyDocument)document;
+                    asmDoc.TraverseOccurrencesWithAction(action, recursive);
+                    break;
+                case DocumentTypeConstants.igPartDocument:
+                    // Process sheet metal document with action delegate
+                    var sheetMetalDoc = (SheetMetalDocument)document;
+                    action(sheetMetalDoc);
+                    break;
+            }
+        }
+    }
+    // TODO: Implement unified PartMetal type traversal once adapter is implemented
+    #endregion
 }
