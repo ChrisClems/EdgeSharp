561c0d509d261ab9e1b77a14f2452933997c867b
ChrisClems <neilclemons@gmail.com>
2024-06-11 07:42:59 -0700

    Expand AssemblyExtensions and add DocumentExtensions
    The AssemblyExtensions class is expanded to include methods for traversing an assembly document with multiple actions and removing parts based on property key-value pairs. A new class, DocumentExtensions, is introduced which contains a method for converting the properties of a Solid Edge document into a nested dictionary. This aids the extraction and manipulation of the various properties associated with Solid Edge documents.

--- EdgeSharp/Extensions/AssemblyExtensions.cs
+++ EdgeSharp/Extensions/AssemblyExtensions.cs
@@ -8,7 +8,12 @@ namespace EdgeSharp.Extensions;
 [SuppressMessage("ReSharper", "SuspiciousTypeConversion.Global")]
 public static class AssemblyExtensions
 {
-    // Process all Solid Edge documents in assembly using delegate action
+    /// <summary>
+    /// Traverses an assembly document and performs an action on Solid Edge documents using a delegate.
+    /// </summary>
+    /// <param name="asm">The assembly document to traverse.</param>
+    /// <param name="action">The action to perform on Solid Edge documents.</param>
+    /// <param name="recursive">Optional. Indicates whether the traversal should be performed recursively in child assembly documents. Default is true.</param>
     public static void TraverseAssemblyWithAction(this AssemblyDocument asm, Action<SolidEdgeDocument> action, bool recursive)
     {
         var occurrences = asm.Occurrences;
@@ -22,6 +27,76 @@ public static class AssemblyExtensions
             {
                 var asmDoc = (AssemblyDocument)document;
                 asmDoc.TraverseAssemblyWithAction(action, recursive);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Traverses an assembly document and performs an action on Solid Edge documents using a delegate.
+    /// </summary>
+    /// <param name="asm">The assembly document to traverse.</param>
+    /// <param name="actions">The actions to perform on Solid Edge documents.</param>
+    /// <param name="recursive">Optional. Indicates whether the traversal should be performed recursively in child assembly documents. Default is true.</param>
+    public static void TraverseAssemblyWithAction(this AssemblyDocument asm, Action<SolidEdgeDocument>[] actions, bool recursive)
+    {
+        var occurrences = asm.Occurrences;
+        foreach (Occurrence occurrence in occurrences)
+        {
+            var document = (SolidEdgeDocument)occurrence.OccurrenceDocument;
+
+            foreach (var action in actions)
+            {
+                action(document);
+            }
+
+            if (recursive && document.Type == DocumentTypeConstants.igAssemblyDocument)
+            {
+                var asmDoc = (AssemblyDocument)document;
+                asmDoc.TraverseAssemblyWithAction(actions, recursive);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Removes parts from an assembly document based on a given property key-value pair.
+    /// </summary>
+    /// <param name="asm">The assembly document.</param>
+    /// <param name="kvp">The key-value pair representing the property to match.</param>
+    /// <param name="caseSensitive">Optional. Indicates whether the property comparison should be case-sensitive. Default is true.</param>
+    /// <param name="recursive">Optional. Indicates whether the removal should be performed recursively in child assembly documents. Default is true.</param>
+    public static void RemovePartsByProperty(this AssemblyDocument asm, KeyValuePair<string, string> kvp, bool caseSensitive = true, bool recursive = true)
+    {
+        var valueMatch = false;
+        var occurrences = asm.Occurrences;
+        foreach (Occurrence occurrence in occurrences)
+        {
+            var document = (SolidEdgeDocument)occurrence.OccurrenceDocument;
+
+            var propSets = document.PropertySetsToDictionary();
+
+            foreach (var propSet in propSets)
+            {
+                foreach (var prop in propSet.Value)
+                {
+                    var keyMatch = caseSensitive ? kvp.Key.Equals(prop.Key) : kvp.Key.Equals(prop.Key, StringComparison.OrdinalIgnoreCase);
+                    if (keyMatch)
+                    {
+                        valueMatch = caseSensitive
+                            ? kvp.Value.Equals(prop.Value) :
+                            kvp.Value.Equals(prop.Value, StringComparison.OrdinalIgnoreCase);
+                    }
+                }
+            }
+
+            if (valueMatch)
+            {
+                occurrence.Delete();
+            }
+
+            if (recursive && document.Type == DocumentTypeConstants.igAssemblyDocument)
+            {
+                var asmDoc = (AssemblyDocument)document;
+                asmDoc.RemovePartsByProperty(kvp, caseSensitive, recursive);
             }
         }
     }
---
+++ EdgeSharp/Extensions/DocumentExtensions.cs
@@ -0,0 +1,49 @@
+﻿using SolidEdgeFramework;
+
+namespace EdgeSharp.Extensions;
+
+public static class DocumentExtensions
+{
+    /// <summary>
+    /// Converts the properties of a SolidEdgeDocument to a nested dictionary.
+    /// </summary>
+    /// <param name="doc">The SolidEdgeDocument whose properties will be converted.</param>
+    /// <param name="extendedProps">An optional dictionary of extended properties to be included in the result.</param>
+    /// <returns>A nested dictionary of the properties. The outer dictionary contains the property set names as keys, and the inner dictionaries contain the property names and values.</returns>
+    public static Dictionary<string, Dictionary<string, string?>> PropertySetsToDictionary(this SolidEdgeDocument doc, Dictionary<string, string>? extendedProps = null)
+    {
+        // Parent dictionary to hold all properties
+        var propertySetDict =
+            new Dictionary<string, Dictionary<string, string?>>();
+        var propertySets = (PropertySets)doc.Properties;
+        
+        foreach (Properties properties in propertySets)
+        {
+            var propertyDict = new Dictionary<string, string?>();
+            foreach (Property property in properties)
+                try
+                {
+                    propertyDict.Add(property.Name, property.get_Value().ToString());
+                }
+                catch (Exception e) when (e is NullReferenceException | e is ArgumentNullException)
+                {
+                    // Ignore broken custom props with null keys.
+                }
+            try
+            {
+                propertySetDict.Add(properties.Name, propertyDict);
+            }
+            // Don't know how this happened. Should not be possible.
+            // If it happens, report as issue: https://github.com/ChrisClems/EdgeSharp/issues
+            catch (Exception ex) when (e is NullReferenceException | e is ArgumentNullException)
+            {
+                throw new Exception(
+                        $"PropertySet Dict Key null exception on {doc.Name}. Please report as issue with SE document file attached: https://github.com/ChrisClems/EdgeSharp/issues ", ex);
+            }
+        }
+        // Optionally add an extra set of custom properties as an ExtendedProperties dictionary
+        if ( extendedProps == null ) return propertySetDict;
+        propertySetDict.Add("ExtendedProperties", extendedProps!);
+        return propertySetDict;
+    }
+}
