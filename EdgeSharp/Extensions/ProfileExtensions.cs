bc5f5c64e3317b9f0f580057f7944cacff375047
ChrisClems <neilclemons@gmail.com>
2024-07-30 11:13:43 -0700

    Add retry logic for Profile and TextProfile clipboard operations
    Introduced `PasteRetry` in `ProfileExtensions` and `CopyRetry` in `TextProfileExtensions` to handle clipboard exceptions. These methods allow for a configurable number of retries and delay intervals to manage COM exceptions related to clipboard access.

---
+++ EdgeSharp/Extensions/ProfileExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using SolidEdgePart;
+using System.Threading;
+using Thread = System.Threading.Thread;
+
+namespace EdgeSharp.Extensions;
+
+public static class ProfileExtensions
+{
+    public static void PasteRetry(this Profile profile, int retryCount = -1, int retryDelay = 100)
+    {
+        const uint CLIPBRD_E_CANT_OPEN = 0x800401D0;
+        while (retryCount != 0)
+        {
+            try
+            {
+                profile.Paste();
+                break;
+            }
+            catch (COMException ex)
+            {
+                if ((uint)ex.ErrorCode != CLIPBRD_E_CANT_OPEN)
+                {
+                    throw new Exception(
+                        "Error pasting text profile. Try opening and closing the text profile dialog in Solid Edge to fix and re-run.",
+                        ex);
+                }
+                if (retryCount > 0)
+                {
+                    retryCount--;
+                }
+                Thread.Sleep(retryDelay);
+            }
+        }
+    }
+}
---
+++ EdgeSharp/Extensions/TextProfileExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using SolidEdgeFrameworkSupport;
+
+namespace EdgeSharp.Extensions;
+
+public static class TextProfileExtensions
+{
+    public static void CopyRetry(this TextProfile textProfile, int retryCount = -1, int retryDelay = 100)
+    {
+        const uint CLIPBRD_E_CANT_OPEN = 0x800401D0;
+        while (retryCount != 0)
+        {
+            try
+            {
+                textProfile.Copy();
+                break;
+            }
+            catch (COMException ex)
+            {
+                if ((uint)ex.ErrorCode != CLIPBRD_E_CANT_OPEN)
+                {
+                    throw new Exception(
+                        "Error copying text profile. Try opening and closing the text profile dialog in Solid Edge to fix and re-run.",
+                        ex);
+                }
+                if (retryCount > 0)
+                {
+                    retryCount--;
+                }
+                Thread.Sleep(retryDelay);
+            }
+        }
+    }
+}
