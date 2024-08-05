460588fe8fcc14235065d08af856b381e23cdfc7
ChrisClems <neilclemons@gmail.com>
2024-08-05 09:11:19 -0700

    Initial code for easier handling of StructuralFrame sections. StructuralFrame extenion will return a list of a new StructuralFrameSection object which exposes data as properties which usually requires use of some very clunky out syntax. Initial exposed properties are angles, orientation vectors, and occurrence objects.

---
+++ EdgeSharp/Extensions/StructuralFrameExtensions.cs
@@ -0,0 +1,27 @@
+﻿using SolidEdgeAssembly;
+
+namespace EdgeSharp.Extensions;
+
+public static class StructuralFrameExtensions
+{
+    public static int[] GetSectionIds(this StructuralFrame frame)
+    {
+        var sectionCount = 0;
+        Array sections = new int[1];
+        frame.GetSections(out sectionCount, ref sections);
+        return (int[])sections;
+    }
+
+    public static List<StructuralFrameSection> GetStructuralFrameSections(this StructuralFrame frame)
+    {
+        var sectionList = new List<StructuralFrameSection>();
+        var sectionIds = GetSectionIds(frame);
+        foreach (var sectionId in sectionIds)
+        {
+            var section = new StructuralFrameSection(frame, sectionId);
+            sectionList.Add(section);
+        }
+
+        return sectionList;
+    }
+}
---
+++ EdgeSharp/Types.cs
@@ -0,0 +1,54 @@
+﻿using SolidEdgeAssembly;
+
+namespace EdgeSharp;
+
+public class Coordinate
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+    
+}
+public class StructuralFrameSection
+{
+    public int SectionId { get; private set; }
+    public Occurrence SectionOccurrence;
+    public int OccurrenceId = 0;
+    public double EndAngle1 = 0;
+    public double EndAngle2 = 0;
+    public double SideAngle1 = 0;
+    public double SideAngle2 = 0;
+    public double Length = 0;
+    //public static double ExactLength = 0;
+    public Coordinate Offset;
+    public Coordinate Rotation;
+
+    internal StructuralFrameSection(StructuralFrame frame, int sectionId)
+    {
+        SectionId = sectionId;
+        Offset = new Coordinate();
+        Rotation = new Coordinate();
+        object objOccurenceId = 0;
+        var objOccurrence = new object();
+        object xOffsetObj = 0.0;
+        object yOffsetObj = 0.0;
+        object zOffsetObj = 0.0;
+        object xRotationObj = 0.0;
+        object yRotationObj = 0.0;
+        object zRotationObj = 0.0;
+        frame.ReturnOccurrenceForGivenSectionID(SectionId, out objOccurenceId, out objOccurrence);
+        SectionOccurrence = (Occurrence)objOccurrence;
+        OccurrenceId = (int)objOccurenceId;
+        frame.GetPlaneOrientationForGivenSectionID(SectionId, out xOffsetObj, out yOffsetObj, out zOffsetObj, out xRotationObj, out yRotationObj, out zRotationObj);
+        Offset.X = (double)xOffsetObj;
+        Offset.Y = (double)yOffsetObj;
+        Offset.Z = (double)zOffsetObj;
+        Rotation.X = (double)xRotationObj;
+        Rotation.Y = (double)yRotationObj;
+        Rotation.Z = (double)zRotationObj;
+        
+        frame.SegmentCutLength(SectionId, out Length, out _);
+        frame.EndFaceEndAngle(SectionId, out EndAngle1, out EndAngle2);
+        frame.SideFaceEndAngle(SectionId, out SideAngle1, out SideAngle2);
+    }
+}
